966213f516ed6392b568cab062308a4051b8cbf9
Jose <jose01.arteaga@gmail.com>
2018-09-12 09:22:37 -0600

    nav drawer

--- app/src/main/java/com/applaudostudio/weekthreechallengeone/MainActivity.java
+++ app/src/main/java/com/applaudostudio/weekthreechallengeone/MainActivity.java
@@ -1,12 +1,22 @@
 package com.applaudostudio.weekthreechallengeone;
 
 import android.content.res.Resources;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-    String[] planets;
-    Resources res;
+
+    private String[] mPlanetTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+//TODO Change menu items and toolbar icons
 
 
     @Override
@@ -14,7 +24,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        res = getResources();
-        planets = res.getStringArray(R.array.planets_array);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_launcher_foreground);
+
+
+        mDrawerLayout = findViewById(R.id.nav_view);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
